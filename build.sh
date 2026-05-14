53ee3e8d83ed66709a13712bf7312152bc6b2765
tamrat123 <tamrat123@example.com>
2026-05-14 21:14:38 +0300

    Automate database migration and data loading in build script

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+if [ -f data.json ]; then
+    echo "Loading data from data.json..."
+    python manage.py loaddata data.json
+fi
