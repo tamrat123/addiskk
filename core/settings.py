71c0fe63cd4be7784563dc364ffa32b0ad155fa4
tamrat123 <tamrat123@example.com>
2026-05-15 09:51:55 +0300

    Ensure explicit database selection for Vercel/Neon

--- core/settings.py
+++ core/settings.py
@@ -87,14 +87,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('POSTGRES_URL', os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')),
-        conn_max_age=600
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+elif os.environ.get('POSTGRES_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(env='POSTGRES_URL', conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
